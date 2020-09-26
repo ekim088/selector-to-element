1d2d27a6fd7fb57ae0d995d5cbf3e3bf236b2d16
Edward Kim <edward@cyberbird.co>
2020-09-26 18:38:01 -0500

    build: adding method to parse attributes

---
+++ src/parseAttributes.ts
@@ -0,0 +1,27 @@
+/**
+ * Parses attributes from a CSS selector.
+ *
+ * @param {string} selector A CSS selector.
+ * @returns {{Object.<string, string>}} An dictionary of the attributes in the
+ *	CSS selector.
+ */
+const parseAttributes = (selector: string): { [name: string]: string } => {
+	const attributes: { [name: string]: string } = {};
+	const matches = selector.match(/\[([\w="':-]*?)\]/g);
+
+	if (matches) {
+		// iterate over matches and build attributes map
+		matches.forEach(match => {
+			// remove matched brackets and split on assignment
+			const [name, value = ''] = match.replace(/[[\]]/g, '').split('=');
+
+			// remove quotes from value if present
+			const formattedValue = value.replace(/['"]/g, '');
+			attributes[name] = formattedValue;
+		});
+	}
+
+	return attributes;
+};
+
+export default parseAttributes;
---
+++ tests/parseAttributes.test.ts
@@ -0,0 +1,62 @@
+import parseAttributes from '../src/parseAttributes';
+
+describe('parseAttributes', () => {
+	it('should parse attributes', () => {
+		expect(parseAttributes('[someAttr=someValue]')).toEqual({
+			someAttr: 'someValue'
+		});
+		expect(parseAttributes('[someAttr="someValue"]')).toEqual({
+			someAttr: 'someValue'
+		});
+		expect(parseAttributes('[someAttr]')).toEqual({
+			someAttr: ''
+		});
+		expect(parseAttributes('[someAttr=someValue][a=b][1=2]')).toEqual({
+			'1': '2',
+			a: 'b',
+			someAttr: 'someValue'
+		});
+		expect(parseAttributes('[a-b=c-d]')).toEqual({
+			'a-b': 'c-d'
+		});
+	});
+
+	it('should parse an attribute attached to a tag', () => {
+		expect(parseAttributes('div[a=b]')).toEqual({
+			a: 'b'
+		});
+	});
+
+	it('should parse an attribute attached to an ID', () => {
+		expect(parseAttributes('#myId[a=b]')).toEqual({
+			a: 'b'
+		});
+	});
+
+	it('should parse an attribute attached to a class', () => {
+		expect(parseAttributes('.a.b[c=d]')).toEqual({
+			c: 'd'
+		});
+	});
+
+	it('should parse an attribute attached to a pseudo-class', () => {
+		expect(parseAttributes('[a=b]:last-child')).toEqual({
+			a: 'b'
+		});
+	});
+
+	it('should parse an attribute attached to a pseudo-element', () => {
+		expect(parseAttributes('[a=b]::before')).toEqual({
+			a: 'b'
+		});
+	});
+
+	it('should support colons in attributes', () => {
+		expect(parseAttributes('[:someAttr=some:value]')).toEqual({
+			':someAttr': 'some:value'
+		});
+		expect(parseAttributes('[:someAttr=some:value]:not(p)')).toEqual({
+			':someAttr': 'some:value'
+		});
+	});
+});
